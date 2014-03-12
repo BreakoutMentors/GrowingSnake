bc7599e4293f87b417129fea161cbc225e2ca5b5
bskinny129 <brian.skinner9@gmail.com>
2014-03-12 11:33:52 -0700

    Update and rename snake.py to snake finished.py

--- snake finished.py
+++ snake finished.py
@@ -3,16 +3,61 @@ pygame.init()
 pygame.key.set_repeat(20, 20)
 screen = pygame.display.set_mode([660,480])
 
+
+#################FINISHED FUNCTIONS#################
+
 def is_touching_snake(x,y):
-    #look at each snake part to see if it is at the x and y position
+    #looks at each snake part to see if it is at the x and y position
     for w in range(0,len(snake_grid_x)):
         if y == snake_grid_y[w] and x == snake_grid_x[w]:
             return True
     
     return False
+
+
+def make_new_dot():
+    global dot_grid_x,dot_grid_y #this allows us to change these variables inside the function
+    
+    dot_grid_x = random.randint(0,21)
+    dot_grid_y = random.randint(0,15)
+
+    #if the new dot location is on top of the snake, keep picking a new location until you get one that works
+    while is_touching_snake(dot_grid_x,dot_grid_y):
+        dot_grid_x = random.randint(0,21)
+        dot_grid_y = random.randint(0,15)
+
+
+def snake_hit_itself():
+    #checks one snake part at a time, to see if the front has hit that snake part
+    for i in range(1,len(snake_grid_x)):   
+        if snake_grid_x[0] == snake_grid_x[i] and snake_grid_y[0] == snake_grid_y[i]:
+            return True
+        
+    #if it finished the for loop without returning True, it should return False (snake didn't hit itself)
+    return False
+
+
+def draw_everything():
+    screen.fill(white)
+    label = myfont.render("Score:"+ " " + str(score) , 1, pygame.color.THECOLORS['blue'])
+    screen.blit(label, (0, 0))
+    for i in range(0,len(snake_grid_x)):
+        #figures out where to draw each snake part (converts grid location to pixel location)
+        snake_part_pixel_x = snake_grid_x[i] * square_size + square_size/2
+        snake_part_pixel_y = snake_grid_y[i] * square_size + square_size/2
+        pygame.draw.circle(screen, green, [snake_part_pixel_x, snake_part_pixel_y], square_size/2, 0)
+    pygame.draw.circle(screen, dark_blue, [dot_grid_x * square_size + square_size/2, dot_grid_y * square_size + square_size/2], square_size/2, 0)    
+    pygame.display.update()
+    
+
+####################################################
+
+
+
+################FUNCTIONS YOU FINISH################
             
 def move_snake():
-    #move each snake part (starting from the back) to where the one in front of it is
+    #moves each snake part (starting from the back) to where the one in front of it is
     for x in range(1,len(snake_grid_x)):
         part = len(snake_grid_x) - x
         snake_grid_x[part] = snake_grid_x[part -1]
@@ -27,63 +72,53 @@ def move_snake():
         snake_grid_y[0] = snake_grid_y[0] - 1
     elif snake_direction == "down":
         snake_grid_y[0] = snake_grid_y[0] + 1
-    
-def make_new_dot():
-    global dot_grid_x,dot_grid_y #this allows us to change these variables inside the function
-    
-    dot_grid_x = random.randint(0,21)
-    dot_grid_y = random.randint(0,15)
-
-    #if the new dot location is on top of the snake, keep picking a new location until you get one that works
-    while is_touching_snake(dot_grid_x,dot_grid_y):
-        dot_grid_x = random.randint(0,21)
-        dot_grid_y = random.randint(0,15)
 
 
 def grow_snake():
+    #use the list's append function to add to the snake_grid_x and snake_grid_y lists
+    #it should add at the position of the last items in the list
+    
     last_snake_part_x = snake_grid_x[len(snake_grid_x) -1]
     last_snake_part_y = snake_grid_y[len(snake_grid_y) -1]
     snake_grid_x.append(last_snake_part_x) #append is a function that adds to the end of a list
     snake_grid_y.append(last_snake_part_y)
 
+
 def snake_touching_dot():
+    #If the front of the snake is in the same place as the dot then return True,
+    #otherwise return False
+    
     if dot_grid_y == snake_grid_y[0] and dot_grid_x == snake_grid_x[0]:
         return True
     else:
         return False
 
+
 def snake_hit_wall():
     #if the snake has left the game boundaries (22 squares wide by 16 high)
+    #then return True, otherwise return False
+
     if snake_grid_x[0] > 21 or snake_grid_x[0] < 0 or snake_grid_y[0] > 15 or snake_grid_y[0] < 0:
         return True
     else:
         return False
 
-def snake_hit_itself():
-    #check one snake part at a time, to see if the front has hit that snake part
-    for x in range(1,len(snake_grid_x)):   
-        if snake_grid_x[0] == snake_grid_x[x] and snake_grid_y[0] == snake_grid_y[x]:
-            return True
-        
-    #if it finished the for loop without returning True, it should return False (snake didn't hit itself)
-    return False
 
 def snake_has_died():
+    #if either snake_hit_wall() or snake_hit_itself() return True, this function should return True,
+    #otherwise return False
+
     if snake_hit_wall() or snake_hit_itself():
         return True
     else:
         return False
 
-def draw_everything():
-    screen.fill(white)
-    label = myfont.render("Score:"+ " " + str(score) , 1, pygame.color.THECOLORS['blue'])
-    screen.blit(label, (0, 0))
-    for i in range(0,len(snake_grid_x)):
-        snake_part_pixel_x = snake_grid_x[i] * square_size + square_size/2
-        snake_part_pixel_y = snake_grid_y[i] * square_size + square_size/2
-        pygame.draw.circle(screen, green, [snake_part_pixel_x, snake_part_pixel_y], square_size/2, 0)
-    pygame.draw.circle(screen, dark_blue, [dot_grid_x * square_size + square_size/2, dot_grid_y * square_size + square_size/2], square_size/2, 0)    
-    pygame.display.update()
+
+##################################################
+
+
+
+
 
 #create variables
 white = [255, 255, 255]
@@ -100,7 +135,7 @@ dot_grid_y = 0
 make_new_dot() #call the function to create a new dot
 
 
-
+#game loop
 running = True
 while running:
     for event in pygame.event.get():
@@ -108,6 +143,7 @@ while running:
             running = False
        
         if event.type == pygame.KEYDOWN:
+            #if an arrow key was pressed, change the snake_direction variable
             if event.key == pygame.K_LEFT and snake_direction != "right":
                 snake_direction = "left"
             elif event.key == pygame.K_RIGHT and snake_direction != "left":
@@ -121,7 +157,7 @@ while running:
 
     move_snake()
 
-    #if the snake eats the dot, create a new dot location, increase your score, and grow the snake
+    #if the snake hits the dot, create a new dot location, increase your score, and grow the snake
     if snake_touching_dot():
         make_new_dot()
         score = score + 1
